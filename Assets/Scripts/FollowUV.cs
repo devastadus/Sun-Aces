b2aac0be0f471718ec88fb02602a87c3bdaec60d
Scott Price <scottprice7@gmail.com>
2016-03-13 20:35:09 -0700

    adding star field

---
+++ Assets/Scripts/FollowUV.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowUV : MonoBehaviour {
+
+    public float offsetSpeed = 10f;
+    private MeshRenderer mr;
+    public float parralax = 2f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mr = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Material mat = mr.material;
+	    Vector2 offset = mat.mainTextureOffset;
+	    offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+        mat.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUV : MonoBehaviour {
+
+    public float offsetSpeed = 10f;
+    private MeshRenderer mr;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mr = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Material mat = mr.material;
+	    Vector2 offset = mat.mainTextureOffset;
+	    offset.x += Time.deltaTime /10f;
+	    mat.mainTextureOffset = offset;
+	}
+}
