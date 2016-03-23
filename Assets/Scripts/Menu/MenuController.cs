8b67d1775f35df7bd2c75d6ca7933f2269604459
devastadus <scottprice7@gmail.com>
2016-03-23 07:56:25 -0700

    updates

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public Toggle player1;
+	public Toggle player2;
+	public Button start;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	
+	}
+
+	public void buttonClick(){
+		Debug.Log("bam");
+	}
+}
