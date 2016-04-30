ed1d9cb4d86a6f4e847f30ed3ba0dc06e05ca447
Scott Price <scottprice7@gmail.com>
2016-04-29 19:13:44 -0700

    update

--- Assets/Scripts/Menu/playerCamera.cs
+++ Assets/Scripts/Menu/playerCamera.cs
@@ -4,17 +4,35 @@ using UnityEngine.UI;
 
 public class playerCamera : MonoBehaviour
 {
-    public Toggle playerToggle;
+    public Toggle Player1Toggle;
+    public Toggle Player2Toggle;
+
+    public GameObject Player1Camera;
+    public GameObject Player2Camera;
 	// Use this for initialization
 	void Start ()
 	{
+        Player1Camera.SetActive(false);
+        Player2Camera.SetActive(false);
+        Player1Toggle.onValueChanged.AddListener(Player1Callback);
+        Player2Toggle.onValueChanged.AddListener(Player2Callback);
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        Debug.Log(playerToggle.isOn);
-        gameObject.SetActive(playerToggle.isOn);
+       // Debug.Log(playerToggle.isOn);
+       // gameObject.SetActive(playerToggle.isOn);
 	}
+
+    public void Player1Callback(bool b)
+    {
+        Player1Camera.SetActive(b);
+    }
+
+    public void Player2Callback(bool b)
+    {
+        Player2Camera.SetActive(b);        
+    }
 }
