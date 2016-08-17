8967e19512f48dce724980166c182a9c3f71f088
Scott Price <scott.price@cigna.com>
2016-08-17 15:11:45 -0700

    fixing background updating rewire

---
+++ Assets/Scripts/Arena/Camera/WarpObjectController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpObjectController : MonoBehaviour {
+
+	private float timmer;
+	private float arenaRange = 10f;
+
+	// Use this for initialization
+	void Start () {
+		timmer = Time.time;	
+	}
+
+	public void Warper(GameObject obj, Collider2D collider){
+		if (timmer+ .1f < Time.time)
+		{
+			Vector3 temp = Vector3.zero;
+			temp = collider.transform.position;
+			Vector2 velocity = collider.gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+			if (obj.name == "Top")
+			{
+		
+				temp.y = obj.transform.position.y;
+				temp.y = temp.y - (arenaRange * 4) + 1f;
+			}
+			else if (obj.name == "Bottom")
+			{
+				temp.y = obj.transform.position.y;
+				temp.y = temp.y + (arenaRange * 4) - 1f;
+			}
+			else if (obj.name == "Right")
+			{
+				temp.x = obj.transform.position.x;
+				temp.x = temp.x - (arenaRange * 4) + 1f;
+			}
+			else if (obj.name == "Left")
+			{
+				temp.x = obj.transform.position.x;
+				temp.x = temp.x + (arenaRange * 4) - 1f;
+			}
+			collider.transform.position = temp;
+			timmer = Time.time + .1f;
+		}
+	}		
+}
--- Assets/Scripts/Arena/Camera/WrapObject.cs
+++ Assets/Scripts/Arena/Camera/WrapObject.cs
@@ -5,16 +5,14 @@ using System.Collections;
 public class WrapObject : MonoBehaviour
 {
 
-    private float arenaRange = 9f;
-    private float time;
+    //private float arenaRange = 9f;    
+	private WarpObjectController controller;
 
     // Use this for initialization
     void Start ()
-	{
-	    time = Time.time;
-	    Debug.Log(time);
+	{	    
+		controller = gameObject.GetComponentInParent<WarpObjectController>();
 	    //Camera mainCamera = Camera.main;
-
 	}
 	
 	// Update is called once per frame
@@ -24,47 +22,10 @@ public class WrapObject : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (time > Time.time + .1f)
-        {
-            Vector3 temp = Vector3.zero;
-            temp = other.transform.position;
-            Vector2 velocity = other.gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
-            if (gameObject.name == "Top")
-            {
-                if (!(velocity.y <= 0))
-                {
-                    temp.y = transform.position.y;
-                    temp.y = temp.y - (arenaRange * 4) + 1f;
-                }
-            }
-            else if (gameObject.name == "Bottom")
-            {
-                if (!(velocity.y >= 0))
-                {
-                    temp.y = transform.position.y;
-                    temp.y = temp.y + (arenaRange * 4) - 1f;
-                }
-            }
-            else if (gameObject.name == "Right")
-            {
-                if (!(velocity.x <= 0))
-                {
-                    temp.x = transform.position.x;
-                    temp.x = temp.x - (arenaRange * 4) + 1f;
-                }
-            }
-            else if (gameObject.name == "Left")
-            {
-                if (!(velocity.x >= 0))
-                {
-                    temp.x = transform.position.x;
-                    temp.x = temp.x + (arenaRange * 4) - 1f;
-                }
-            }
-            other.transform.position = temp;
-            time = Time.time + .1f;
-        }
+		controller.Warper(gameObject,other);
+	}
 
-    }
-        
+	void OnTriggerStay2D(Collider2D other) {
+		controller.Warper(gameObject,other);
+	}
 }
