1e327bd83ac8eb8b8b0675e7adcd0a40c5b47a2b
Scott Price <scottprice7@gmail.com>
2016-08-30 19:20:22 -0700

    Updates to rewire + music

---
+++ Assets/Scripts/Arena/Controllers/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    audioSource = GetComponent<AudioSource>();
+	    int i = Random.Range(0, clips.Length);
+	    audioSource.clip = clips[i];
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
