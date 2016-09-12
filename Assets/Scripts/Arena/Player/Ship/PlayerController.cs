02ff7d888ee878a194e92ba42afd523ce8375971
Scott Price <scott.price@cigna.com>
2016-09-12 14:19:36 -0700

    making player controller

---
+++ Assets/Scripts/Arena/Player/Ship/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class PlayerController : MonoBehaviour {
+	public int PlayerId; // The Rewired player id of this character
+	protected Rewired.Player _player; // The Rewired Player
+
+	void Awake()
+	{
+		_player = ReInput.players.GetPlayer(PlayerId);
+	}
+
+	public Rewired.Player GetPlayer()
+	{
+		return _player;
+	}
+
+	// Use this for initialization
+	protected void Start () {
+		if(PlayerId == 0)
+			gameObject.layer = LayerMask.NameToLayer("Player1");
+		if(PlayerId == 1)
+			gameObject.layer = LayerMask.NameToLayer("Player2");	
+	}
+
+}
--- Assets/Scripts/Arena/Player/Ship/RedShipController.cs
+++ Assets/Scripts/Arena/Player/Ship/RedShipController.cs
@@ -1,55 +1,45 @@
 ﻿using UnityEngine;
-using Rewired;
 
-	public class RedShipController : MonoBehaviour {	        		
-        public float Speed = 200f; 
-        public float RotateSpeed = 80f;
-        public float MaxSpeed = 8;
-		public int PlayerId; // The Rewired player id of this character
-        float _thrust;
-        float _rotate;
 
-		private Rewired.Player _player; // The Rewired Player
-        //bool isCurrentlyTrusting = false;
-        Rigidbody2D _rigidBody2D;
+public class RedShipController : PlayerController {	        		
+	public float Speed = 200f; 
+	public float RotateSpeed = 80f;
+	public float MaxSpeed = 8;
+	float _thrust;
+	float _rotate;
 
-		void Awake()
-		{
-			_player = ReInput.players.GetPlayer(PlayerId);
-		}
+	//bool isCurrentlyTrusting = false;
+	Rigidbody2D _rigidBody2D;
 
-		public Rewired.Player GetPlayer()
-		{
-			return _player;
-		}
-		        	
-        // Use this for initialization
-        void Start () {
-            _rigidBody2D = gameObject.GetComponent<Rigidbody2D>();			
-        }
-	
-        // Update is called once per frame
-        void FixedUpdate () {
-            Move();
-            Rotate();				
-        }
+	// Use this for initialization
+	void Start () {
+		base.Start();
+		_rigidBody2D = gameObject.GetComponent<Rigidbody2D>();	
 
-        void Move(){		
+	}
 
-            _thrust = _player.GetAxis("Vertical");
-            _rotate = _player.GetAxis("Horizontal");
-            _rigidBody2D.AddForce(transform.rotation * Vector2.up *_thrust * Speed * Time.deltaTime);
-            Vector2 limit = Vector2.ClampMagnitude(_rigidBody2D.velocity,MaxSpeed);
-            _rigidBody2D.velocity = limit;	
-        }
+	// Update is called once per frame
+	void FixedUpdate () {
+		Move();
+		Rotate();				
+	}
 
-        void Rotate(){
-					
-            //Torque
-            _rigidBody2D.AddTorque(-_rotate * Time.deltaTime *RotateSpeed);
-            _rigidBody2D.AddForce(transform.rotation * Vector2.up *_thrust * Speed * Time.deltaTime);
-            Vector2 limit = Vector2.ClampMagnitude(_rigidBody2D.velocity,MaxSpeed);
-            _rigidBody2D.velocity = limit;	
-        }
-    }
+	void Move(){		
+
+		_thrust = _player.GetAxis("Vertical");
+		_rotate = _player.GetAxis("Horizontal");
+		_rigidBody2D.AddForce(transform.rotation * Vector2.up *_thrust * Speed * Time.deltaTime);
+		Vector2 limit = Vector2.ClampMagnitude(_rigidBody2D.velocity,MaxSpeed);
+		_rigidBody2D.velocity = limit;	
+	}
+
+	void Rotate(){
+
+		//Torque
+		_rigidBody2D.AddTorque(-_rotate * Time.deltaTime *RotateSpeed);
+		_rigidBody2D.AddForce(transform.rotation * Vector2.up *_thrust * Speed * Time.deltaTime);
+		Vector2 limit = Vector2.ClampMagnitude(_rigidBody2D.velocity,MaxSpeed);
+		_rigidBody2D.velocity = limit;	
+	}
+}
 
